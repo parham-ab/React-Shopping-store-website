19ca055c60c460909832ed02229b5ba4ce4faf69
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-31 14:55:29 +0430

    fonts added

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -4,10 +4,24 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // Styles
 import "./assets/styles/index.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Quicksand"].join(","),
+    fontSize: 15,
+    fontWeightLight: 400,
+    fontWeightMedium: 500,
+    fontWeightRegular: 600,
+    fontWeightBold: 700,
+  },
+});
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
