ef53b0050fe7cad329619bf9fe540ae2d08fdca9
Parham Abolghasemi <parhamab17@gmail.com>
2022-09-08 09:07:47 +0430

    navbar bugs fixed

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import {
+  Typography,
+  Toolbar,
+  AppBar,
+  Badge,
+  IconButton,
+  Container,
+} from "@mui/material";
 // icons
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 // context
 import { CardContext } from "../../contexts/CardContextProvider";
-import { Typography, Toolbar, AppBar, Badge, IconButton } from "@mui/material";
 
 const Navbar = () => {
   const { state } = useContext(CardContext);
@@ -15,26 +22,27 @@ const Navbar = () => {
         backgroundColor: "#b7b7b7",
       }}
     >
-      <Toolbar
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          boxShadow: "rgb(131 131 131 / 20%) 0px 20px 25px",
-        }}
-      >
-        <Link to="/products">
-          <Typography variant="h6" component="div" color="text.secondary">
-            Store
-          </Typography>
-        </Link>
-        <Link to="/cards">
-          <IconButton>
-            <Badge badgeContent={state.itemsCounter} color="info">
-              <ShoppingCartIcon color="action" sx={{ fontSize: "30px" }} />
-            </Badge>
-          </IconButton>
-        </Link>
-      </Toolbar>
+      <Container maxWidth="lg">
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link to="/products">
+            <Typography variant="h6" component="div" color="text.secondary">
+              Store
+            </Typography>
+          </Link>
+          <Link to="/cards">
+            <IconButton>
+              <Badge badgeContent={state.itemsCounter} color="info">
+                <ShoppingCartIcon color="action" sx={{ fontSize: "30px" }} />
+              </Badge>
+            </IconButton>
+          </Link>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
